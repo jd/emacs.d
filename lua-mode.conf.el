1bbaaa885f75d9a2d423ba00baada45353cd6982
Julien Danjou <julien@danjou.info>
2012-07-24 22:48:58 +0200

    Fix lua-mode configuration
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ lua-mode.conf.el
@@ -0,0 +1,2 @@
+(setq lua-indent-level 4)
+(define-key lua-mode-map (kbd "RET") 'newline-and-indent)
--- lua.conf.el
+++
@@ -1,6 +0,0 @@
-(setq lua-indent-level 4)
-
-;; Lua mode is crap and setup a keymap each time you active the mode
-(add-hook 'lua-mode-hook (lambda ()
-                           (define-key lua-mode-map (kbd "RET")
-                             'newline-and-indent)))
