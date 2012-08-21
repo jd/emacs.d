f78a3c764dc582d0ce839c0d8be3767337ee9114
Julien Danjou <julien@danjou.info>
2012-08-21 10:19:10 +0200

    Fix solar conf

--- solar.conf.el
+++ solar.conf.el
@@ -1,3 +1,4 @@
 ;; Set time display format
-(setcar (memq '12-hours calendar-time-display-form) '24-hours)
-(delq 'am-pm calendar-time-display-form)))
+(unless (memq '24-hours calendar-time-display-form)
+  (setcar (memq '12-hours calendar-time-display-form) '24-hours))
+(delq 'am-pm calendar-time-display-form)
