aab73385e7d78306803e077e01327a591498c803
Julien Danjou <julien@danjou.info>
2012-08-29 15:42:17 +0200

    gnus-notifications: now part of Gnus
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-clock-persist-query-save t)
+ '(org-clock-sound t)
+ '(org-show-notification-handler (quote notifications-notify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
