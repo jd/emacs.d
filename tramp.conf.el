c0a5f1914b4c8cf6d388ad9737156a5217a546e1
Julien Danjou <julien@danjou.info>
2012-11-08 17:58:10 +0100

    Remove sudo stuff

--- tramp.conf.el
+++ tramp.conf.el
@@ -1,1 +1,2 @@
-(add-to-list 'tramp-default-proxies-alist '(".*" "\\`.+\\'" "/ssh:%h:"))
+;; This is supposed to make sudo work, but it breaks /ssh:user@hostname:/
+;; (add-to-list 'tramp-default-proxies-alist '(".*" "\\`.+\\'" "/ssh:%h:"))
