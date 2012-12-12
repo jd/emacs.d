2410a4a669d0a6a267a02c09b3dc12099f1cad4f
Julien Danjou <julien@danjou.info>
2012-12-12 17:03:07 +0100

    Fix SQL mode

--- sql.conf.el
+++ sql.conf.el
@@ -1,1 +1,3 @@
-(setq sql-product 'postgres)            ; That's what I do the most.
+(add-hook 'sql-mode-hook
+          (defun jd:sql-mode-set-sql-product ()
+            (sql-set-product 'postgres))) ; That's what I do the most.mode.conf
