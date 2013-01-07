fb170678e2976f7d8178c3e1886874ae4f62d0a6
Julien Danjou <julien@danjou.info>
2013-01-07 17:18:06 +0100

    Stop using deprecated variable
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- compile.conf.el
+++ compile.conf.el
@@ -1,7 +1,7 @@
 (require 'notifications)
 (setq compilation-auto-jump-to-first-error t)
 (setq compilation-scroll-output 'first-error)
-(setq compilation-finish-function
-      (defun jd:compilation-finish-function (buf end)
-        (notifications-notify :title "Compilation finished"
-                              :body end)))
+(add-to-list 'compilation-finish-functions
+             (defun jd:compilation-finish-function (buf end)
+               (notifications-notify :title "Compilation finished"
+                                     :body end)))
