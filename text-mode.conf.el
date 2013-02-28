f4cd23cb1cc5f78855772b5e2d7c725e78cb85a4
Julien Danjou <julien@danjou.info>
2013-02-28 11:02:53 +0100

    Fix org-mode and TODO
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- text-mode.conf.el
+++ text-mode.conf.el
@@ -1,5 +1,6 @@
 (add-hook 'text-mode-hook
-          (lambda ()
-            (jd:font-lock-add-hack-keywords)
+          (defun jd:text-mode-hook ()
+            (unless (eq major-mode 'org-mode)
+              (jd:font-lock-add-hack-keywords))
             (auto-fill-mode 1)
             (use-hard-newlines 1 'never)))
