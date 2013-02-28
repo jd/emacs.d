6e480d09280d81425f8929a7e8e11158e1c430ff
Julien Danjou <julien@danjou.info>
2013-02-28 11:03:17 +0100

    Remove useless font-lock, enhance regexp
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- jd-font-lock.el
+++ jd-font-lock.el
@@ -2,7 +2,7 @@
 (defun jd:font-lock-add-hack-keywords ()
   (font-lock-add-keywords
    nil
-   '(("\\<\\(FIXME\\|HACK\\|XXX\\|TODO\\|NOTE\\)"
+   '(("\\<\\(FIXME\\|HACK\\|XXX\\|TODO\\|NOTE\\)\\>"
       1
       '(:box (:color "grey10" :line-width 2) :background "red" :bold t :foreground "yellow")
       prepend))))
