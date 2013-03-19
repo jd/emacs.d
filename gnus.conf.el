595935b693c0f09547aaa3cb03a59e6c601bb97c
Julien Danjou <julien@danjou.info>
2013-03-19 12:34:48 +0100

    Add Approved too

--- gnus.conf.el
+++ gnus.conf.el
@@ -79,6 +79,7 @@ http://lists.debconf.org/mailman/listinfo/\\1"))
                               ("Patch Set [[:digit:]]: I would prefer that you didn't merge this" 0 0 error)
                               ("Patch Set [[:digit:]]+: Works for me" 0 0 success)
                               ("Patch Set [[:digit:]]+: Verified" 0 0 success)
+                              ("Patch Set [[:digit:]]+: Approved" 0 0 success)
                               ("^.+ has uploaded a new change for review." 0 0 bold)
                               ("Jenkins has submitted this change and it was merged." 0 0 success))))
         ("^lists\\.openstack\\.\\(.+\\)"
