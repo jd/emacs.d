4ae9f01c0da364978a6e78dd90b4f1b840b1334b
Julien Danjou <julien@danjou.info>
2013-04-24 10:38:31 +0200

    Remove track-score

--- erc.conf.el
+++ erc.conf.el
@@ -2,7 +2,6 @@
 (add-to-list 'erc-modules 'match)
 (add-to-list 'erc-modules 'scrolltobottom)
 (add-to-list 'erc-modules 'notifications)
-(add-to-list 'erc-modules 'track-score)
 
 ;; (setq erc-header-line-format "%t: %o")
 (setq erc-header-line-format "")        ; XXX workaround emacs bug, %o not recognize wtf
