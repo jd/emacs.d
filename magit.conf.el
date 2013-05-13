d7ea9d50ab7e5fb3c72f96f5b94e8cae18e3e4ec
Julien Danjou <julien@danjou.info>
2013-05-13 17:37:01 +0200

    Update magit repos

--- magit.conf.el
+++ magit.conf.el
@@ -1,4 +1,4 @@
-(setq magit-repo-dirs `(,jd:projects-directory "~/.emacs.d" "~/Debian" "~/.emacs.d/el-get"))
+(setq magit-repo-dirs `(,jd:projects-directory "~/Debian"))
 (setq magit-commit-signoff t)
 (setq magit-remote-ref-format 'remote-slash-branch)
 (setq magit-completing-read-function 'magit-iswitchb-completing-read)
