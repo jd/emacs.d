1f0f396d815f0c8102b9d30335f91881a632c1f0
Julien Danjou <julien@danjou.info>
2013-06-18 17:18:38 +0200

    Add binding for Slime
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- lisp-mode.conf.el
+++ lisp-mode.conf.el
@@ -1,2 +1,3 @@
 (define-key emacs-lisp-mode-map (kbd "RET") 'newline-and-indent)
 (define-key lisp-mode-map (kbd "RET") 'newline-and-indent)
+(define-key lisp-mode-map (kbd "C-c C-z") 'slime-switch-to-output-buffer)
