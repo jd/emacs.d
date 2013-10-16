daa9f0eeb51ba56e3a007c08b4d120b804d506a6
Julien Danjou <julien@danjou.info>
2013-10-16 12:44:05 +0200

    Fix again?

--- erc-match.conf.el
+++ erc-match.conf.el
@@ -12,15 +12,14 @@ Put each name on a line."
 
 (defun jd:erc-read-file (file)
   (when (file-exists-p file)
-    (mapcar (lambda (nick)
-              (concat "^" nick "$"))
-            (remove-if
-             (lambda (str) (string= str ""))
-             (split-string
-              (with-temp-buffer
-                (insert-file-contents-literally file)
-                (buffer-string))
-              "\n")))))
+    (regexp-opt
+     (remove-if
+      (lambda (str) (string= str ""))
+      (split-string
+       (with-temp-buffer
+         (insert-file-contents-literally file)
+         (buffer-string))
+       "\n")))))
 
 (setq erc-pals (jd:erc-read-file jd:erc-pals-file))
 (setq erc-fools (jd:erc-read-file jd:erc-fools-file))
