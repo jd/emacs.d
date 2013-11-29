eaada4bb10aa3cf0378c03e47590805166c55e37
Julien Danjou <julien@danjou.info>
2013-11-29 16:20:48 +0100

    Revert "Fix SMTP config"
    This reverts commit d147354129022f725a062186cd763b07ec4d219d.

--- smtp.conf.el
+++
@@ -1,3 +0,0 @@
-(setq smtp-server "mail.naquadah.org")
-(setq smtp-use-starttls t)
-(setq smtp-service "submission")
---
+++ smtpmail.conf.el
@@ -0,0 +1,3 @@
+(setq smtpmail-smtp-server "mail.naquadah.org")
+(setq smtpmail-stream-type 'starttls)
+(setq smtpmail-smtp-service "submission")
