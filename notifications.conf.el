d44221a78979001d3912b93169c661b973fe79c8
Julien Danjou <julien@danjou.info>
2013-12-11 17:36:09 +0100

    Add notifications-notify version for darwin

---
+++ notifications.conf.el
@@ -0,0 +1,24 @@
+(when (string-equal system-type "darwin")
+  (defun jd:xml-unescape-string (string)
+    (with-temp-buffer
+    (insert string)
+    (dolist (substitution '(("&amp;" . "&")
+			    ("&lt;" . "<")
+			    ("&gt;". ">")
+			    ("&apos;" . "'")
+			    ("&quot;" . "\"")))
+      (goto-char (point-min))
+      (while (search-forward (car substitution) nil t)
+	(replace-match (cdr substitution) t t nil)))
+    (buffer-string)))
+
+  (defun notifications-notify (&rest params)
+    (let ((title (plist-get params :title))
+          (body (plist-get params :body)))
+      (start-process "terminal-notifier" nil
+                     "terminal-notifier"
+                     "-message" (jd:xml-unescape-string body)
+                     "-title" (jd:xml-unescape-string title)
+                     "-activate" "org.gnu.Emacs"
+                     "-sender" "org.gnu.Emacs"))))
+
