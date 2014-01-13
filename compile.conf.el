9a5618e706d31d451492fbb274c25d596e2594f7
Julien Danjou <julien@danjou.info>
2014-01-13 15:15:36 +0100

    Do not jump on first error, this fails with Python

--- compile.conf.el
+++ compile.conf.el
@@ -1,5 +1,4 @@
 (require 'notifications)
-(setq compilation-auto-jump-to-first-error t)
 (setq compilation-scroll-output 'first-error)
 (add-to-list 'compilation-finish-functions
              (defun jd:compilation-finish-function (buf end)
