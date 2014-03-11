02d1937300ec6cb028671f3eee7772bbaa8b8b18
Julien Danjou <julien@danjou.info>
2014-03-11 11:56:32 +0100

    Update IRC

---
+++ git-gutter.conf.el
@@ -0,0 +1,3 @@
+;; `org-indent-mode' does not play nice with git-gutter, so let's disable
+;; it :-(
+(setq git-gutter:disabled-modes '(org-mode))
--- jd-irc.el
+++ jd-irc.el
@@ -6,6 +6,10 @@
   (erc-tls
    :server "prometheus.naquadah.org"
    :port 6666
+   :nick "jd/bitlbee")
+  (erc-tls
+   :server "prometheus.naquadah.org"
+   :port 6666
    :nick "jd/oftc")
   (erc-tls
    :server "prometheus.naquadah.org"
@@ -40,5 +44,19 @@
               (jd:erc-start))
           (message "Not doing anything."))
       (jd:erc-start))))
+
+;; (setq rcirc-server-alist
+;;       `(("prometheus.naquadah.org"
+;;          :encryption tls
+;;          :port 6666
+;;          :nick "jd/naquadah"
+;;          :password ,(funcall
+;;                      (cadr
+;;                       (memq :secret
+;;                             (car (auth-source-search :host "prometheus.naquadah.org"
+;;                                                      :max 1
+;;                                                      :port 6666
+;;                                                      :user "jd/naquadah"
+;;                                                      :require '(:secret)))))))))
 
 (provide 'jd-irc)
