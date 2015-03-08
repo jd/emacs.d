563308b5954781eb183b3dc0aae3c3acc9091b19
Julien Danjou <julien@danjou.info>
2015-03-08 19:31:40 +0100

    Fix notif if empty

--- notifications.conf.el
+++ notifications.conf.el
@@ -1,25 +1,25 @@
 (when (string-equal system-type "darwin")
   (defun jd:xml-unescape-string (string)
     (with-temp-buffer
-    (insert string)
-    (dolist (substitution '(("&amp;" . "&")
-			    ("&lt;" . "<")
-			    ("&gt;". ">")
-			    ("&apos;" . "'")
-			    ("&quot;" . "\"")))
-      (goto-char (point-min))
-      (while (search-forward (car substitution) nil t)
-	(replace-match (cdr substitution) t t nil)))
-    (buffer-string)))
+      (insert string)
+      (dolist (substitution '(("&amp;" . "&")
+                              ("&lt;" . "<")
+                              ("&gt;". ">")
+                              ("&apos;" . "'")
+                              ("&quot;" . "\"")))
+        (goto-char (point-min))
+        (while (search-forward (car substitution) nil t)
+          (replace-match (cdr substitution) t t nil)))
+      (buffer-string)))
 
   (defun notifications-notify (&rest params)
     (let ((title (plist-get params :title))
           (body (plist-get params :body)))
       (call-process "terminal-notifier" nil nil nil
-                     "terminal-notifier"
-                     "-contentImage" (or (plist-get params :image-path) "")
-                     "-appIcon" (or (plist-get params :app-icon) "")
-                     "-message" (jd:xml-unescape-string body)
-                     "-title" (jd:xml-unescape-string title)
-                     "-activate" "org.gnu.Emacs"
-                     "-sender" "org.gnu.Emacs"))))
+                    "terminal-notifier"
+                    "-contentImage" (or (plist-get params :image-path) "")
+                    "-appIcon" (or (plist-get params :app-icon) "")
+                    "-message" (if body (jd:xml-unescape-string body) "")
+                    "-title" (if title (jd:xml-unescape-string title) "")
+                    "-activate" "org.gnu.Emacs"
+                    "-sender" "org.gnu.Emacs"))))
