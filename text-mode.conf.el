c8adc5633c50d05f162b9c62e8bd30c977b35108
Julien Danjou <julien@danjou.info>
2015-05-26 09:17:39 +0200

    Flyspell on text-mode

--- text-mode.conf.el
+++ text-mode.conf.el
@@ -2,5 +2,6 @@
           (defun jd:text-mode-hook ()
             (unless (eq major-mode 'org-mode)
               (jd:font-lock-add-hack-keywords))
+            (turn-on-flyspell)
             (auto-fill-mode 1)
             (use-hard-newlines 1 'never)))
