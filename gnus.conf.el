cf93d59de2a3f222cfdf9a432913fbd442379e98
Julien Danjou <julien@danjou.info>
2015-09-15 17:33:18 +0200

    Fix OpenStack lists banner

--- gnus.conf.el
+++ gnus.conf.el
@@ -114,9 +114,9 @@ http://lists.debconf.org/mailman/listinfo/\\1"))
         ("^lists\\.openstack\\.\\(.+\\)"
          (to-list . "\\1@lists.openstack.org")
          (list-identifier . "\\\\[\\1\\\\]")
-         (banner . "_______________________________________________
-\\1 mailing list
-\\1@lists.openstack.org
+         (banner . "__________________________________________________________________________
+.+
+Unsubscribe: .+@lists.openstack.org.+
 http://lists.openstack.org/cgi-bin/mailman/listinfo/\\1"))
         ("^lists\\.openstack-fr\\.\\(.+\\)"
          (to-list . "\\1@listes.openstack.fr"))
