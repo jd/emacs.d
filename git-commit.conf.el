3ad12e424d7eb7901d92c00e563e2d380dc9779e
Julien Danjou <julien@danjou.info>
2016-01-14 14:51:25 +0100

    Add Co-Authored-By and others as known git headers

--- git-commit.conf.el
+++ git-commit.conf.el
@@ -1,3 +1,9 @@
 (add-hook 'git-commit-mode-hook (lambda () (toggle-save-place 0))) ; Disable it
 (add-hook 'git-commit-mode-hook 'turn-on-flyspell)
 (setq git-commit-summary-max-length 80)
+(add-to-list 'git-commit-known-pseudo-headers "Co-Authored-By")
+(add-to-list 'git-commit-known-pseudo-headers "Related-Bug")
+(add-to-list 'git-commit-known-pseudo-headers "Closes-Bug")
+(add-to-list 'git-commit-known-pseudo-headers "Implements")
+(add-to-list 'git-commit-known-pseudo-headers "Changed-Id")
+(add-to-list 'git-commit-known-pseudo-headers "Depends-On")
