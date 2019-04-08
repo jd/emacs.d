b0aef71dcc93d730284efe3f493facfef3101c9f
Julien Danjou <julien@danjou.info>
2019-04-08 15:40:21 +0200

    Remove Red Hat stuff

--- message.conf.el
+++ message.conf.el
@@ -8,15 +8,6 @@
                                           "^submit@bugs.debian\\.org$"
                                           "\\)"))
 (setq message-confirm-send t)
-(add-hook 'message-send-hook
-          (defun jd:check-redhat-from-redhat ()
-            (unless (string-match-p "redhat.com" (message-field-value "From"))
-              (when (or (string-match-p "redhat.com" (or (message-field-value "To") ""))
-                        (string-match-p "redhat.com" (or (message-field-value "Cc") "")))
-                ;; I could use `y-or-n-p' but since `message-confirm-send' is
-                ;; enabled, I'm too much used of pressing `y' after C-c C-c.
-                (unless (string= "ok" (read-from-minibuffer "Send message to redhat.com addresses with non redhat.com From? [ok] "))
-                  (keyboard-quit))))))
 
 ;; Kill buffer when message is sent
 (setq message-kill-buffer-on-exit t)
@@ -46,10 +37,3 @@
 (setq smtpmail-smtp-server "smtp.fastmail.com")
 (setq smtpmail-stream-type 'starttls)
 (setq smtpmail-smtp-service "submission")
-
-(setq message-send-mail-function
-      (defun jd:message-smtpmail-send-it ()
-        (if (string-match-p "redhat.com" (message-field-value "From"))
-            (let ((smtpmail-smtp-server "smtp.corp.redhat.com"))
-              (message-smtpmail-send-it))
-          (message-smtpmail-send-it))))
